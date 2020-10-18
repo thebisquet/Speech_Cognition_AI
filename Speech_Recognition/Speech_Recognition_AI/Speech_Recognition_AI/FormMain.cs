aa26f68041caa250360bf2f612f84f61a31dd60f
Keinen Bousquet <keinen.bousquet@trojans.dsu.edu>
2020-10-17 20:49:04 -0500

    Added basic voice recognition and speech feedback ability. Renamed AI Bot to Jarvis.

--- Speech_Recognition/Speech_Recognition_AI/Speech_Recognition_AI/FormMain.cs
+++ Speech_Recognition/Speech_Recognition_AI/Speech_Recognition_AI/FormMain.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
 
 namespace Speech_Recognition_AI
 {
@@ -15,16 +17,80 @@ namespace Speech_Recognition_AI
     {
         //Create object to instantiate a speech synthesizer
         SpeechSynthesizer synth = new SpeechSynthesizer();
+        Choices list = new Choices();
+
+        //Sleep/Wake mode
+        Boolean wake = true;
 
         public FormMain()
         {
-            InitializeComponent();
+            //Initilize new object to process speech
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
 
-            //Set voice to female and test initial voice settings
-            synth.SelectVoiceByHints(VoiceGender.Female);
-            synth.Speak("Hello, It's been awhile since I have seen you. How are you?");
+            //List of possible sayings to expect from the user
+            //WARNING: Must be lowercase, as the computer doesn't correct for case sensitivity
+            list.Add(new string[] { "hello", "hello jarvis", "how are you", "what time is it", "what day is it", "open google", "go to sleep",
+            "jarvis", "restart", "update" });
+
+            //Gather voice information for processing
+            Grammar gramm = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gramm);
+                rec.SpeechRecognized += rec_SpeechRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                return;
+            }
+
+            //Set voice to Male because Jarvis is a man
+            synth.SelectVoiceByHints(VoiceGender.Male);
+
+            InitializeComponent();
+            CenterToScreen();
         }
 
+        public void say(String hi) {
+            synth.Speak(hi);
+        }
+        
+        public void restart() {
+            Process.Start(@"C:\Users\Jarvis\jarvis.exe");
+            Environment.Exit(0);
+        }
+
+        private void rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //Store user voice results in a string for comparisons
+            String user = e.Result.Text;
+
+            //Set wake and sleep functionality
+            if (user == "go to sleep")
+            {
+                say("Love you bro, goodnight.");
+                wake = false;
+            }
+            if (user == "jarvis")
+            {
+                say("Sup dude?");
+                wake = true;
+            }
+
+            //If awake, dictate what to respond to
+            if (wake == true)
+            {
+                if (user == "hello jarvis" || user == "hello") { say("Sup bro?"); }
+                if (user == "how are you") { say("I'm doing good brother, thanks for asking. Is there anything I can do for you?"); }
+                if (user == "what time is it") { say(DateTime.Now.ToString("h:mm tt")); }
+                if (user == "what day is it") { say(DateTime.Now.ToString("dddd M/d/yyyy")); }
+                if (user == "open google") { Process.Start("http://google.com"); }
+                if (user == "restart" || user == "update") { restart(); }
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
